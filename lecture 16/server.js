714e1f82d7997ef65633427a77a39f9e3c0aefcd
riyansh sharma <sharmariyansh2362@gmail.com>
2026-09-18 10:46:17 +0530

    lec 16  commit

--- lecture 16/server.js
+++ lecture 16/server.js
@@ -1,22 +1,42 @@
-const express=require('express');
-const app=express();
-const PORT=3000;
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const PORT = 3000;
 
-const logmiddleware=(req,res,next)=>{  
-    req.name="Riyansh"; 
-    console.log("Request url:",req.url,"req method:",req.method,"Time:",new Date().toLocaleTimeString());
-    //res.send("hello from middleware");
-   next();
-}
+
+const apimiddleware = (req, res, next) => {
+    if (req.query.api_key === "12345") {  
+        console.log("API middleware passed");
+        next();
+    } else {
+        res.send("Invalid API key");
+    }
+};
 
 app.use(logmiddleware);
+app.use(apimiddleware);
+app.use(morgan());
 
-app.get("/",(req,res)=>{
+
+app.get("/", (req, res) => {
     console.log("hello World");
     res.send("hello World");
-})
+});
+
+app.get("/about", (req, res) => {
+    res.send(`Hello ${req.name} from about page`);
+});
+
+app.get("/data", (req, res) => {
+    res.json({
+        city: "Delhi",
+        country: "India",
+        temperature: 30
+    });
+});
 
 
-app.listen(PORT,()=>{
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
